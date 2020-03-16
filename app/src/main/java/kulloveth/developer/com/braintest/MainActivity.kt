64598807fe33ac8c82229011c0605acb3238b087
kulloveth <kulloveth@gmail.com>
2020-03-16 15:48:26 +0100

    setup navigation from chooseSignIn to emailOption

--- app/src/main/java/kulloveth/developer/com/braintest/MainActivity.kt
+++ app/src/main/java/kulloveth/developer/com/braintest/MainActivity.kt
@@ -1,13 +1,24 @@
 package kulloveth.developer.com.braintest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.fragment_choose_sign_in_method.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        emailOption.setOnClickListener {
+            it.findNavController().navigate(R.id.action_chooseSignInMethodFragment_to_emailSignIn)
+        }
+
+        gmailOption.setOnClickListener {
+            Snackbar.make(it, "Google Option is currently unavailable", Snackbar.LENGTH_SHORT)
+                .show()
+        }
     }
 
 }
