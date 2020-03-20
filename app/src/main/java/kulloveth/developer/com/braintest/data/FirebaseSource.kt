6c07ae89416100f06f15faa0c4471197f1aeeb20
kulloveth <kulloveth@gmail.com>
2020-03-20 21:43:35 +0100

    setup googleclient in firebase source

--- app/src/main/java/kulloveth/developer/com/braintest/data/FirebaseSource.kt
+++ app/src/main/java/kulloveth/developer/com/braintest/data/FirebaseSource.kt
@@ -1,10 +1,16 @@
 package kulloveth.developer.com.braintest.data
 
 import android.content.Context
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import io.reactivex.Completable
+import kulloveth.developer.com.braintest.MainActivity
 import kulloveth.developer.com.braintest.R
+import java.lang.ref.WeakReference
 
 class FirebaseSource {
 
@@ -18,6 +24,7 @@ class FirebaseSource {
     companion object {
         private var firebaseSource: FirebaseSource? = null
         private var firebaseAuth: FirebaseAuth? = null
+        private var mClient: WeakReference<GoogleSignInClient>? = null
         fun initializeFirebase() {
             if (firebaseSource == null) {
                 firebaseSource = FirebaseSource()
@@ -25,11 +32,33 @@ class FirebaseSource {
             }
         }
 
-        fun initializeGoogleSignin(context: Context) = Completable.create { notify ->
-            GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+        fun initializeGoogleSignin(context: Context) {
+            val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(context.getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build()
+            mClient = WeakReference(GoogleSignIn.getClient(context, gso))
+        }
+
+        fun getGoogleClient(): GoogleSignInClient? {
+            return mClient?.get()
+        }
+
+        fun firebaseAuthWithGoogle(
+            activity: MainActivity,
+            googleSignInAccount: GoogleSignInAccount
+        ) = Completable.create { notify ->
+            val authCredential = GoogleAuthProvider.getCredential(googleSignInAccount.idToken, null)
+            firebaseAuth?.signInWithCredential(authCredential)?.addOnCompleteListener {
+                if (!notify.isDisposed) {
+                    if (it.isSuccessful) {
+                        notify.onComplete()
+                    } else {
+                        notify.onError(it.exception!!)
+                    }
+                }
+            }
+
         }
 
         fun signIn(email: String, password: String) =
@@ -61,10 +90,12 @@ class FirebaseSource {
 
         }
 
-        fun signOut() = firebaseAuth!!.signOut()
+        fun signOut() {
+            firebaseAuth?.signOut()
+            getGoogleClient()?.signOut()
 
-        fun currenUser() = firebaseAuth!!.currentUser
+            fun currenUser() = firebaseAuth!!.currentUser
+        }
+
+
     }
-
-
-}
